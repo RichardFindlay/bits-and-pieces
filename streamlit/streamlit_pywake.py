676855c5e1c73342d5a9ce59b30fc539a39d3052
Richard Findlay <rfindlay1992@gmail.com>
2022-11-28 23:27:46 +0000

    pywake interface with streamlit

---
+++ streamlit/streamlit_pywake.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+import streamlit.components.v1 as components
+
+
+# load html setup
+
+# filename = st.text_input('/Users/richardfindlay/Documents/Coding/Python/Projects/streamlit_guis/template/index.html')
+
+# try:
+#     with open(filename) as input:
+#         st.text(input.read())
+# except FileNotFoundError:
+#     st.error('File not found.')
+
+
+
+
+
+# html_string = '''
+# <h1>HTML string in RED</h1>
+
+# <script language="javascript">
+#   document.querySelector("h1").style.color = "red";
+#   console.log("Streamlit runs JavaScript");
+#   alert("Streamlit runs JavaScript");
+# </script>
+# '''
+
+components.html(
+    """
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div id="accordion">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h5 class="mb-0">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Collapsible Group Item #1
+            </button>
+          </h5>
+        </div>
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+            Collapsible Group Item #1 content
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h5 class="mb-0">
+            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Collapsible Group Item #2
+            </button>
+          </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div class="card-body">
+            Collapsible Group Item #2 content
+          </div>
+        </div>
+      </div>
+    </div>
+    """,
+    height=600,
+)
+
+
+# components.html(filename)  # JavaScript works
+
+# st.markdown(html_string, unsafe_allow_html=True)  
+
+
